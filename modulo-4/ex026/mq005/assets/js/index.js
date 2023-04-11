fd8a83c7fc402e01e7ec93d3db5fee64bebee333
Augusto Sell dos Santos <105236980+augustosellsantos@users.noreply.github.com>
2023-04-10 22:02:29 -0300

    aplicando js

--- modulo-4/ex026/mq005/assets/js/index.js
+++ modulo-4/ex026/mq005/assets/js/index.js
@@ -1,9 +1,10 @@
 const menuBurger = document.querySelector('.menubruger')
 const menu = document.querySelector('.menu')
-const close = document.querySelector('.close')
+const close1 = document.querySelector('.close')
 
 menuBurger.addEventListener('click', (event) => {
     event.preventDefault()
     menu.classList.toggle('visibility')
-    close.classList.toggle('visibility')
+    close1.classList.toggle('visibility')
+    menuBurger.classList.toggle('menuburger')
 })
